f516bae86ffb941d859368b537eb91af1ee8b7e5
ZhaoWei Xu <paldier@hotmail.com>
2020-09-20 20:58:12 +0800

    add new zlib

--- src/router/rules/zlib.mk
+++ src/router/rules/zlib.mk
@@ -6,7 +6,7 @@ ZLIB_PKG_BUILD_DIR=$(TOP)/zlib
 ZLIB_CMAKE_OPTIONS=-DZLIB_COMPAT=ON
 ZLIB_STAGING_DIR=$(TOP)/_staging/usr
 ZLIB_EXTRA_CFLAGS=-I$(TOP)/_staging/usr/include $(COPTS) $(MIPS16_OPT)
-ZLIB_EXTRA_LDFLAGS=-L$(TOP)/_staging/usr/lib
+ZLIB_EXTRA_LDFLAGS=-L$(TOP)/_staging/usr/lib $(COPTS) $(MIPS16_OPT)
 #ZLIB_EXTRA_ARFLAGS="cru $(LTOPLUGIN)" 
 #RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 ifeq ($(ARCHITECTURE),broadcom)
@@ -15,29 +15,35 @@ ZLIB_EXTRA_CFLAGS+=-DNOTLS
 endif
 endif
 ifeq ($(ARCH),i386)
-ZLIB_CMAKE_OPTIONS+=-DASM686=ON
-endif
+ZLIB_CMAKE_OPTIONS+=-DASM686=ON -DWITH_AVX2=ON -DWITH_SSE2=ON -DWITH_SSSE3=ON -DWITH_SSE4=ON -DWITH_PCLMULQDQ=ON
+else
 ifeq ($(ARCH),x86_64)
-ZLIB_CMAKE_OPTIONS+=-DAMD64=ON
-endif
+ZLIB_CMAKE_OPTIONS+=-DAMD64=ON -DWITH_AVX2=ON -DWITH_SSE2=ON -DWITH_SSSE3=ON -DWITH_SSE4=ON -DWITH_PCLMULQDQ=ON
+else
 ifeq ($(ARCHITECTURE),ipq806x)
 ZLIB_CMAKE_OPTIONS+=-DARMv8=ON -DWITH_NEON=ON
-endif
+else
 ifeq ($(ARCHITECTURE),alpine)
 ZLIB_CMAKE_OPTIONS+=-DARMv8=ON -DWITH_NEON=ON
-endif
+else
 ifeq ($(ARCHITECTURE),ventana)
 ZLIB_CMAKE_OPTIONS+=-DARMv8=ON -DWITH_NEON=ON
+else
+ZLIB_CMAKE_OPTIONS+=-DARMv8=OFF -DWITH_NEON=OFF -DWITH_ACLE=OFF -DWITH_NEON=OFF
+endif
+endif
+endif
+endif
 endif
 
 
-zlib/stamp-h1:
+zlib-configure:
 	$(call CMakeClean,$(ZLIB_PKG_BUILD_DIR))
 	$(call CMakeConfigure,$(ZLIB_PKG_BUILD_DIR),$(ZLIB_STAGING_DIR),$(ZLIB_CMAKE_OPTIONS),$(ZLIB_EXTRA_CFLAGS),$(ZLIB_EXTRA_LDFLAGS)) 
-	touch $@
 
-zlib: zlib/stamp-h1
+zlib: zlib-configure
 	$(MAKE) -C zlib
+	rm -f zlib/libz.a
 
 zlib-install:
 	install -D zlib/libz.so.1.2.11 $(INSTALLDIR)/zlib/usr/lib/libz.so.1.2.11
